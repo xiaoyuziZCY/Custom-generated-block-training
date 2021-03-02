d8b2ad92bd9dcc054c07e82f008d049271158969
xiaoyuziZCY <1162735365@qq.com>
2021-03-02 19:35:50 +0800

    自定义区块训练

--- main.go
+++ main.go
@@ -12,4 +12,6 @@ func main() {
 	fmt.Println("区块hash值是",genesis.Hash)
 	block1 :=chain.CreateBlock(genesis.Height,genesis.Hash,nil)
 	fmt.Println(block1)
+	block2 := chain.CreateBlock(block1.Height,block1.Hash,nil)
+	fmt.Println(block2)
 }
